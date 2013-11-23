c23ae64e0d12663d640e9707c8a5c4dc0ca4d953
Thomas Morris <thomas.imorris@gmail.com>
2013-11-23 08:13:57 +0200

    Add Administrator controller for adding content to site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AndradaPortfolioSite::Application.routes.draw do
+  root 'administrator#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
