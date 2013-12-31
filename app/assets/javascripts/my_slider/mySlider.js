b33da35c5ff35cb2c612cc54c6ccb7fbc74212c4
Thomas Morris <thomas.imorris@gmail.com>
2013-12-31 09:21:51 +0200

    Remove unneccesary comment in mySlider.js

--- app/assets/javascripts/my_slider/mySlider.js
+++ app/assets/javascripts/my_slider/mySlider.js
@@ -6,15 +6,6 @@ $(function(){
   var currentIndex = 0;
   var fadeTime = 700; //the time to fade in and out (in ms)
 
-  // // fade out all except the first slide
-  // slides.each(function( idx, element ){
-  //   var slide = $(element);
-  //   if (slide.index() != 0) {
-  //     // slide.fadeOut();
-  //   }
-  // });
-
-
   /**
    *  changeToSlide()
    *
