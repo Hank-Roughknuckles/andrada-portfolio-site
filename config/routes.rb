1a8effae80edf3d9fbd0ad488c8f7e1c4c3901fd
Thomas Morris <thomas.imorris@gmail.com>
2014-01-03 16:37:17 +0200

    Remove line from when admin controller existed

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ AndradaPortfolioSite::Application.routes.draw do
   get '/login' => 'sessions#new'
   get '/logout' =>  'sessions#destroy'
 
-  #useful for linking to the admin/[page_name] pages
-  get ':controller/:action'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
