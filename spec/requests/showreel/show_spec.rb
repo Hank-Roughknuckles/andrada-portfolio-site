4554d79126a0f1819494cf987e3e223e7519550e
Thomas Morris <thomas.imorris@gmail.com>
2014-01-05 01:54:09 +0200

    Write basic tests for edit page layout

--- spec/requests/showreel/show_spec.rb
+++ spec/requests/showreel/show_spec.rb
@@ -2,14 +2,46 @@ require 'spec_helper'
 
 describe "ShowreelPages" do
   let(:showreel) { FactoryGirl.create :showreel }
+  let(:user) { FactoryGirl.create :user }
   subject { page }
 
+  before do
+    visit showreel_path showreel.id
+  end
+
+  describe "Showreel nav button" do
+    before do
+      visit root_path
+      click_link "Showreel"
+    end
+
+    it { should have_title "Showreel" }
+  end
+  
   describe "show page contents" do
     before do
       visit showreel_path showreel.id
     end
 
     it { should have_title "Showreel" }
+    it { should have_content showreel.header }
+    it { should have_content showreel.description }
+
+    describe "The Edit Button" do
+      describe "when not logged in" do
+        it { should_not have_content "Edit Content" }
+      end
+
+      describe "when logged in" do
+        before do
+          login_as user
+          visit showreel_path showreel.id
+        end
+
+        it { should have_content "Edit Content" }
+        
+      end
+    end
   end
   
 end
