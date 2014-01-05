321eda8abea2a044e98c115674eb8f57417efcc6
Thomas Morris <thomas.imorris@gmail.com>
2014-01-05 16:14:27 +0200

    Add validation to model   -Presence of description, video_link   -format of video link must be vimeo

--- app/models/showreel.rb
+++ app/models/showreel.rb
@@ -1,2 +1,6 @@
 class Showreel < ActiveRecord::Base
+  VideoLinkRegex = /[a-zA-Z:\/]+vimeo.com\/[0-9]+/i
+
+  validates_presence_of :description, :video_link
+  validates_format_of :video_link, :with => VideoLinkRegex
 end
