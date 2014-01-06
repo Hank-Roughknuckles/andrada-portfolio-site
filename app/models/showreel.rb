7b031a592c689589e385c2193acd95362960c3c7
Thomas Morris <thomas.imorris@gmail.com>
2014-01-06 14:33:37 +0200

    Add whitespace stripping for video ilnk

--- app/models/showreel.rb
+++ app/models/showreel.rb
@@ -3,4 +3,10 @@ class Showreel < ActiveRecord::Base
 
   validates_presence_of :description, :video_link
   validates_format_of :video_link, :with => VideoLinkRegex
+  before_validation :strip_whitespace
+
+  private
+  def strip_whitespace
+    self.video_link = self.video_link.strip
+  end
 end
