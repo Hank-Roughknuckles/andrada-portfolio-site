cd1e73f23c9c5427d96a7d82e1bc833a9a5cb899
Thomas Morris <thomas.imorris@gmail.com>
2014-01-10 08:21:28 +0200

    Make embed_video able to handle null inputs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,12 @@ class ApplicationController < ActionController::Base
   # given a link to a vimeo video, return the id number for the video to
   # be used in the embed code in the view
   def embed_video( link, options = {} )
-    vimeo_id = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.match(link)[6]
+    vimeo_id = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.match(link)
+    if vimeo_id
+      vimeo_id = vimeo_id[6]
+    else
+      return "Bad Video Link"
+    end
     width = options[:width] || 500
     height = options[:height] || 375
     return "<iframe src=\"//player.vimeo.com/video/#{vimeo_id}\" width=\"#{width}\"
