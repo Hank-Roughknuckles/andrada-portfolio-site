adb34921c8871d3a7fb04802b0293b10e7536bfa
Thomas Morris <thomas.imorris@gmail.com>
2014-01-16 08:36:55 +0200

    Add media_choice to showreel and works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,12 +1,12 @@
 class Work < ActiveRecord::Base
   VideoLinkRegex = /[a-zA-Z:\/]*(vimeo|youtube)\.com\/.+/i
 
-  validates_presence_of :description, :video_link
-  validates_format_of :video_link, :with => VideoLinkRegex
+  validates_presence_of :media_choice
+  validates_format_of :media_link, :with => VideoLinkRegex
   before_validation :strip_whitespace
 
   private
   def strip_whitespace
-    self.video_link = self.video_link.strip
+    self.media_link = self.media_link.strip
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115065900) do
+ActiveRecord::Schema.define(version: 20140116063334) do
 
   create_table "about_me_contents", force: true do |t|
     t.string   "header"
@@ -50,11 +50,12 @@ ActiveRecord::Schema.define(version: 20140115065900) do
   end
 
   create_table "showreels", force: true do |t|
-    t.string   "video_link"
+    t.string   "media_link"
     t.string   "header"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "media_choice"
   end
 
   create_table "users", force: true do |t|
@@ -76,11 +77,12 @@ ActiveRecord::Schema.define(version: 20140115065900) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "works", force: true do |t|
-    t.string   "video_link"
+    t.string   "media_link"
     t.string   "header"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "media_choice"
   end
 
 end
