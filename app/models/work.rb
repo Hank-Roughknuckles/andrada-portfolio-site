53b5fee4b95b2a7775bd06b12d7f8f3e41a7e666
Thomas Morris <thomas.imorris@gmail.com>
2014-01-21 08:43:09 +0200

    Change showreel and works models to pass tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,8 @@ class Work < ActiveRecord::Base
   validates_presence_of :media_choice
   validates_format_of :media_link, :with => VideoLinkRegex
   before_validation :strip_whitespace
-  has_attached_file :media_image, :background_image
+  has_attached_file :media_image
+  has_attached_file :background_image
 
   private
   def strip_whitespace
