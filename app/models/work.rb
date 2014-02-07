ad92887378c1e0420bb469f47da928d726504b34
Thomas Morris <thomas.imorris@gmail.com>
2014-02-07 10:02:39 +0200

    Remove comment

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,7 +7,6 @@ class Work < ActiveRecord::Base
   has_attached_file :media_image
   has_attached_file :grid_tile_image, :styles => { :small => "100x100#", :large => "500x500>" }, :processors => [:cropper]
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h 
-  # after_update :reprocess_grid_tile_image, :if => :cropping?
 
   def cropping?
     !crop_x.blank? && !crop_y.blank? && !crop_h.blank? && !crop_w.blank?
