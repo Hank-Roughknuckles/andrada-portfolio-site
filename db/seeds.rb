0df1422565482699402d8c8044f5743b8806baa8
Thomas Morris <thomas.imorris@gmail.com>
2014-02-10 16:48:58 +0200

    Remove obsolete navItem code from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 
 User.create( id:0, email: "asdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf" )
 
-NavItem.create(id:0, link_1_name: "About Me", link_2_name: "Showreel", link_3_name: "Works", link_4_name: "Current Projects", link_5_name: "Contact")
-
 AboutMeContent.create(id: 0, header: "Who I Am", description: "Lorem ipsum sit dolor amet", button_title: "Intro")
 AboutMeContent.create(id: 1, header: "What I Do", description: "Lorem ipsum sit dolor amet", button_title: "Qualifications")
 AboutMeContent.create(id: 2, header: "What I Love", description: "Lorem ipsum sit dolor amet", button_title: "Bio")
