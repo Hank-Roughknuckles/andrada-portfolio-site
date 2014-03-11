93e99aa094b6acdf835ef7d92da2970dfe51b16f
Thomas Morris <thomas.imorris@gmail.com>
2014-03-11 14:05:06 -0500

    Add background image to contact page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307160906) do
+ActiveRecord::Schema.define(version: 20140311190046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20140307160906) do
     t.string   "vimeo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "background_image_file_name"
+    t.string   "background_image_content_type"
+    t.integer  "background_image_file_size"
+    t.datetime "background_image_updated_at"
   end
 
   create_table "current_projects", force: true do |t|
