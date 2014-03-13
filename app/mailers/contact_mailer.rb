5fc394ec5d747f372d3205485f81014db4e2b5b6
Thomas Morris <thomas.imorris@gmail.com>
2014-03-12 19:31:11 -0500

    Add contact_email method to mailer controller

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default to: User.first.email
+
+  def contact_email(email, body)
+    @email = email
+    @body = body
+
+    mail(from: email, subject: "Message from your portfolio site")
+  end
 end
